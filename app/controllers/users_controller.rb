65015a3bbd8b1ced972f5354717ae301f68d76ac
EC2 Default User <ec2-user@ip-172-31-41-234.ap-northeast-1.compute.internal>
2020-06-02 05:24:49 +0000

    編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
   def show
     @users = User.all
-    @approval = User(params[:id])
+    @approval = User.where(superior: true)
     respond_to do |format|
       format.html do
           #html用の処理を書く
