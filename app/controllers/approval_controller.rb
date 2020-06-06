795d0565096b9818845aae119d86e48654ed25f3
EC2 Default User <ec2-user@ip-172-31-41-234.ap-northeast-1.compute.internal>
2020-06-06 15:02:02 +0000

    一ヶ月申請画面編集

--- app/controllers/approval_controller.rb
+++
@@ -1,3 +0,0 @@
-class ApprovalController < ApplicationController
-    
-end
--- app/models/approval.rb
+++
@@ -1,8 +0,0 @@
-class Approval < ApplicationRecord
-  belongs_to :user
-  
-  validates :month, presence: true
-  validates :decision, presence: true
-  validates :authorizer, presence: true
-  
-end
